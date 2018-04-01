b402f4374686acccc4c0aea1d44bc646bbcad314
Matthew Chidlow <8273507+BLUESHADOW2020@users.noreply.github.com>
2018-04-01 22:11:57 +0800

    Added debug method for printing vk return codes easier, and note for potential improvement

--- src/utility/DebugUtility.h
+++ src/utility/DebugUtility.h
@@ -1,12 +1,16 @@
 #ifndef DEBUG_UTILITY_H
 #define DEBUG_UTILITY_H
 
+#include <vulkan/vulkan.h>
 #include <iostream>
+#include <sstream>
 
 #include <glm/glm.hpp>
 
 class DebugUtility{
 public:
+
+    //TODO disable when not debug?
 
     static void print(const char* name, glm::mat4 &mat4){
         std::cout << "----" << name << "----" << std::endl;
@@ -15,6 +19,23 @@ public:
                 std::cout << mat4[i][j] << (j != 3 ? " " : "");
             }
             std::cout << std::endl;
+        }
+    }
+
+    static void VkSuccess(VkResult result, std::string message){
+
+        if (result != VK_SUCCESS){
+            std::stringstream stream;
+
+            stream
+                << message
+                << "\n"
+                << "Error: \n"
+                << result
+                << "\n";
+
+            throw std::runtime_error(stream.str());
+
         }
     }
 
