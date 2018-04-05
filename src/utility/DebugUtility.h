9f9910f26ea437d1eb8115c4591939b870db9ba8
Matthew Chidlow <8273507+BLUESHADOW2020@users.noreply.github.com>
2018-04-05 19:11:16 +0800

    Improved VkSuccess to also tell you the name of the error.

--- src/utility/DebugUtility.h
+++ src/utility/DebugUtility.h
@@ -22,17 +22,51 @@ public:
         }
     }
 
+    static void ifSet(VkResult result, VkResult desiredResult, std::string name, std::string& storageString){
+        if (result == desiredResult){
+            storageString = name;
+        }
+    }
+
     static void VkSuccess(VkResult result, std::string message){
 
         if (result != VK_SUCCESS){
             std::stringstream stream;
 
+            std::string errorName;
+
+            ifSet(result, VK_NOT_READY, "VK_NOT_READY", errorName);
+            ifSet(result, VK_TIMEOUT, "VK_TIMEOUT", errorName);
+            ifSet(result, VK_EVENT_SET, "VK_EVENT_SET", errorName);
+            ifSet(result, VK_EVENT_RESET, "VK_EVENT_RESET", errorName);
+            ifSet(result, VK_INCOMPLETE, "VK_INCOMPLETE", errorName);
+            ifSet(result, VK_ERROR_OUT_OF_HOST_MEMORY, "VK_ERROR_OUT_OF_HOST_MEMORY", errorName);
+            ifSet(result, VK_ERROR_OUT_OF_DEVICE_MEMORY, "VK_ERROR_OUT_OF_DEVICE_MEMORY", errorName);
+            ifSet(result, VK_ERROR_INITIALIZATION_FAILED, "VK_ERROR_INITIALIZATION_FAILED", errorName);
+            ifSet(result, VK_ERROR_DEVICE_LOST, "VK_ERROR_DEVICE_LOST", errorName);
+            ifSet(result, VK_ERROR_MEMORY_MAP_FAILED, "VK_ERROR_MEMORY_MAP_FAILED", errorName);
+            ifSet(result, VK_ERROR_LAYER_NOT_PRESENT, "VK_ERROR_LAYER_NOT_PRESENT", errorName);
+            ifSet(result, VK_ERROR_EXTENSION_NOT_PRESENT, "VK_ERROR_EXTENSION_NOT_PRESENT", errorName);
+            ifSet(result, VK_ERROR_FEATURE_NOT_PRESENT, "VK_ERROR_FEATURE_NOT_PRESENT", errorName);
+            ifSet(result, VK_ERROR_INCOMPATIBLE_DRIVER, "VK_ERROR_INCOMPATIBLE_DRIVER", errorName);
+            ifSet(result, VK_ERROR_TOO_MANY_OBJECTS, "VK_ERROR_TOO_MANY_OBJECTS", errorName);
+            ifSet(result, VK_ERROR_FORMAT_NOT_SUPPORTED, "VK_ERROR_FORMAT_NOT_SUPPORTED", errorName);
+            ifSet(result, VK_ERROR_FRAGMENTED_POOL, "VK_ERROR_FRAGMENTED_POOL", errorName);
+            ifSet(result, VK_ERROR_OUT_OF_POOL_MEMORY, "VK_ERROR_OUT_OF_POOL_MEMORY", errorName);
+            ifSet(result, VK_ERROR_INVALID_EXTERNAL_HANDLE, "VK_ERROR_INVALID_EXTERNAL_HANDLE", errorName);
+            ifSet(result, VK_ERROR_SURFACE_LOST_KHR, "VK_ERROR_SURFACE_LOST_KHR", errorName);
+            ifSet(result, VK_ERROR_NATIVE_WINDOW_IN_USE_KHR, "VK_ERROR_NATIVE_WINDOW_IN_USE_KHR", errorName);
+            ifSet(result, VK_SUBOPTIMAL_KHR, "VK_SUBOPTIMAL_KHR", errorName);
+            ifSet(result, VK_ERROR_OUT_OF_DATE_KHR, "VK_ERROR_OUT_OF_DATE_KHR", errorName);
+            ifSet(result, VK_ERROR_INCOMPATIBLE_DISPLAY_KHR, "VK_ERROR_INCOMPATIBLE_DISPLAY_KHR", errorName);
+            ifSet(result, VK_ERROR_VALIDATION_FAILED_EXT, "VK_ERROR_VALIDATION_FAILED_EXT", errorName);
+            ifSet(result, VK_ERROR_INVALID_SHADER_NV, "VK_ERROR_INVALID_SHADER_NV", errorName);
+            ifSet(result, VK_ERROR_NOT_PERMITTED_EXT, "VK_ERROR_NOT_PERMITTED_EXT", errorName);
+
             stream
-                << message
-                << "\n"
-                << "Error: \n"
-                << result
-                << "\n";
+                << message << "\n"
+                << "Error Code: " << result << "\n"
+                << "Error Name: " << errorName << "\n";
 
             throw std::runtime_error(stream.str());
 
